0e8559b82886f74e04ccc7f40d734c363fb12a5a
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-11-03 00:38:21 +0530

    checking Last

--- src/test/java/bridgelabz/exception/ExceptionUserRegistrationTest.java
+++ src/test/java/bridgelabz/exception/ExceptionUserRegistrationTest.java
@@ -1,5 +1,39 @@
 package bridgelabz.exception;
 
+package com.bdlz.exceptionuser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ExceptionUserRegistrationTest {
 
+    ExceptionUserRegistration exceptionUserRegistration = new ExceptionUserRegistration();
+
+    @Test
+    public void givenFirstName_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.firstName("Ramakrishna");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenFirstName_IsProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.firstName("RamaKrishna");
+        Assert.assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void givenLastName_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.firstName("Velisetti");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenLastName_IsProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.firstName("VeLiseTTi");
+        Assert.assertEquals(false, actualResult);
+    }
+
 }
+ 
