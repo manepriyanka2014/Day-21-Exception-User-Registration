b9d003ef81e4d82bae763fb000793f2a0f189603
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-11-03 00:51:05 +0530

    checking for Mobile Number

--- src/test/java/bridgelabz/exception/ExceptionUserRegistrationTest.java
+++ src/test/java/bridgelabz/exception/ExceptionUserRegistrationTest.java
@@ -7,44 +7,54 @@ import static org.junit.Assert.*;
 
 public class ExceptionUserRegistrationTest {
 
-    ExceptionUserRegistration exceptionUserRegistration = new ExceptionUserRegistration();
+	ExceptionUserRegistration exceptionUserRegistration = new ExceptionUserRegistration();
 
-    @Test
-    public void givenFirstName_IsProper_ReturnTrue() {
-        boolean actualResult = exceptionUserRegistration.firstName("Ramakrishna");
-        Assert.assertEquals(true, actualResult);
-    }
+	@Test
+	public void givenFirstName_IsProper_ReturnTrue() {
+		boolean actualResult = exceptionUserRegistration.firstName("Ramakrishna");
+		Assert.assertEquals(true, actualResult);
+	}
 
-    @Test
-    public void givenFirstName_IsProper_ReturnFalse() {
-        boolean actualResult = exceptionUserRegistration.firstName("RamaKrishna");
-        Assert.assertEquals(false, actualResult);
-    }
+	@Test
+	public void givenFirstName_IsProper_ReturnFalse() {
+		boolean actualResult = exceptionUserRegistration.firstName("RamaKrishna");
+		Assert.assertEquals(false, actualResult);
+	}
 
-    @Test
-    public void givenLastName_IsProper_ReturnTrue() {
-        boolean actualResult = exceptionUserRegistration.lastName("Velisetti");
-        Assert.assertEquals(true, actualResult);
-    }
+	@Test
+	public void givenLastName_IsProper_ReturnTrue() {
+		boolean actualResult = exceptionUserRegistration.lastName("Velisetti");
+		Assert.assertEquals(true, actualResult);
+	}
 
-    @Test
-    public void givenLastName_IsProper_ReturnFalse() {
-        boolean actualResult = exceptionUserRegistration.lastName("VeLiseTTi");
-        Assert.assertEquals(false, actualResult);
-    }
+	@Test
+	public void givenMobileNumber_IsProper_ReturnTrue() {
+		boolean actualResult = exceptionUserRegistration.mobileNumber("91 9989774456");
+		Assert.assertEquals(true, actualResult);
+	}
 
-    @Test
-    public void givenEmailId_IsProper_ReturnTrue() {
-        boolean actualResult = exceptionUserRegistration.emailId("ramakrishna96.velisetti@gmail.com");
-        Assert.assertEquals(true, actualResult);
-    }
+	@Test
+	public void givenMobileNumber_IsNotProper_ReturnFalse() {
+		boolean actualResult = exceptionUserRegistration.mobileNumber("919989774456");
+		Assert.assertEquals(false, actualResult);
+	}
 
-    @Test
-    public void givenEmailId_IsNotProper_ReturnFalse() {
-        boolean actualResult = exceptionUserRegistration.emailId("ramakrishna96.velisettigmail.com");
-        Assert.assertEquals(false, actualResult);
-    }
+	@Test
+	public void givenLastName_IsProper_ReturnFalse() {
+		boolean actualResult = exceptionUserRegistration.lastName("VeLiseTTi");
+		Assert.assertEquals(false, actualResult);
+	}
+
+	@Test
+	public void givenEmailId_IsProper_ReturnTrue() {
+		boolean actualResult = exceptionUserRegistration.emailId("ramakrishna96.velisetti@gmail.com");
+		Assert.assertEquals(true, actualResult);
+	}
+
+	@Test
+	public void givenEmailId_IsNotProper_ReturnFalse() {
+		boolean actualResult = exceptionUserRegistration.emailId("ramakrishna96.velisettigmail.com");
+		Assert.assertEquals(false, actualResult);
+	}
 
 }
-
- 
