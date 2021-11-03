c4794e613f8c66d707cffb71eea19c5f85415865
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-11-03 12:29:29 +0530

    Added User Password Rule 1

--- src/main/java/bridgelabz/exception/ExceptionUserRegistration.java
+++ src/main/java/bridgelabz/exception/ExceptionUserRegistration.java
@@ -34,6 +34,12 @@ public class ExceptionUserRegistration {
         Matcher matcher = pattern.matcher(mobileNumber);
         return matcher.matches();
     }
-}
 
+    public boolean passwordRule1(String password) {
+        regex = "^[a-z]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+}
  
